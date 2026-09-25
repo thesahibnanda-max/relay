6dd4c147ee799e1d086183425f7290b41a8a3a53
Sahib Nanda <284372108+thesahibnanda-max@users.noreply.github.com>
2026-09-25 10:26:25 +0530

    feat: load server/.env.stage via godotenv on startup

--- server/main.go
+++ server/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log/slog"
+
 	"go.uber.org/fx"
 
 	"github.com/thesahibnanda-max/relay/server/package/app"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(".env.stage"); err != nil {
+		slog.Warn("error in setting env vars", slog.Any("error", err))
+	}
 	fx.New(
 		app.Module,
 		fx.Invoke(app.Serve),
